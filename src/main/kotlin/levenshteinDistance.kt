983aa20e3b60da3810400033c65516dd2d68012d
Samuel Soukup <samuel.soukup@jetbrains.com>
2023-09-22 10:39:55 +0200

    Refactor levenshteinDistance function to use getEdits method

--- src/main/kotlin/levenshteinDistance.kt
+++ src/main/kotlin/levenshteinDistance.kt
@@ -1,27 +1,5 @@
 import kotlinx.serialization.Serializable
 
-// get levenstein distance between two strings
-fun <T> levenshteinDistance(s1: List<T>, s2: List<T>): Int {
-    val len1 = s1.size
-    val len2 = s2.size
-    val dp = Array(len1 + 1) { IntArray(len2 + 1) }
-    for (i in 0..len1) {
-        dp[i][0] = i
-    }
-    for (j in 0..len2) {
-        dp[0][j] = j
-    }
-    for (i in 1..len1) {
-        for (j in 1..len2) {
-            val left = dp[i - 1][j] + 1
-            val down = dp[i][j - 1] + 1
-            val leftDown = dp[i - 1][j - 1]
-            dp[i][j] = minOf(left, down, leftDown + if (s1[i - 1] == s2[j - 1]) 0 else 1)
-        }
-    }
-    return dp[len1][len2]
-}
-
 @Serializable
 sealed interface TextEdit {
     val index: Int
@@ -33,10 +11,10 @@ data class Insert(override val index: Int, val text: List<String>) : TextEdit {
     override fun toString(): String {
         return "I($text @ $index)"
     }
-
     override fun apply(input: List<String>, index: Int): List<String> {
         return input.subList(0, index) + text + input.subList(index, input.size)
     }
+
 }
 
 @Serializable
@@ -44,10 +22,10 @@ data class Delete(override val index: Int, val text: List<String>) : TextEdit {
     override fun toString(): String {
         return "D($text @ $index)"
     }
-
     override fun apply(input: List<String>, index: Int): List<String> {
         return input.subList(0, index) + input.subList(index + text.size, input.size)
     }
+
 }
 
 @Serializable
@@ -55,10 +33,10 @@ data class Replace(override val index: Int, val oldText: List<String>, val newTe
     override fun toString(): String {
         return "R($oldText -> $newText @ $index)"
     }
-
     override fun apply(input: List<String>, index: Int): List<String> {
         return input.subList(0, index) + newText + input.subList(index + oldText.size, input.size)
     }
+
 }
 
 fun getEdits(s1: List<String>, s2: List<String>): List<TextEdit> {
@@ -102,9 +80,6 @@ fun getEdits(s1: List<String>, s2: List<String>): List<TextEdit> {
 
 }
 
-fun main() {
-    val s1 = splitString("Tohle vymažu něco tu zůstane tohle nahradím.")
-    val s2 = splitString("něco tu zůstane tamto vyhradím. bafity baf")
-    println(getEdits(s1, s2))
-    println(levenshteinDistance(s1, s2))
+fun levenshteinDistance(s1: List<String>, s2: List<String>): Int {
+    return getEdits(s1, s2).size
 }
