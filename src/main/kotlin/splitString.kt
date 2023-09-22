14babc8699c99bae89bfadf67295f0b51c1ee34d
Samuel Soukup <samuel.soukup@jetbrains.com>
2023-09-22 10:45:40 +0200

    Add documentation

--- src/main/kotlin/splitString.kt
+++ src/main/kotlin/splitString.kt
@@ -1,3 +1,19 @@
+/**
+ * Splits the given input string based on various delimiters and returns a list of strings.
+ *
+ * This splits the input string based on the following delimiters:
+ * - whitespace
+ * - punctuation
+ * - capital letters
+ * - special characters
+ *
+ * So this splits tokens by whitespaces as expected, but also splits tokens like "HelloWorld" into "Hello" and "World".
+ * This is done to make the program synthesis algorithm more robust to camelCase and snake_case.
+ *
+ *
+ * @param inputString the string to be split
+ * @return a list of strings after splitting the input string
+ */
 fun splitString(inputString: String): List<String> {
     return inputString
         .split("([_.,;~ˇ^˘°˛`˙˝¨\"\'\\s])|(?=\\p{Lu})|(?=\\p{Punct})|(?<=\\p{Punct})".toRegex())
